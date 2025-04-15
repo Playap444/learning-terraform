c441cf35583fd94c0d968e8f2ce7f737303edcf2
christian.blue <christian.blue@13luellc.com>
2025-04-14 22:13:58 -0400

    fix

--- variables.tf
+++ variables.tf
@@ -2,6 +2,16 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
+# variable "client_id" {
+#   type = string
+# }
+# variable "client_secret" {
+#   type = string
+#   sensitive = true
+# }
+# variable "tenant_id" {
+#   type = string
+# }
